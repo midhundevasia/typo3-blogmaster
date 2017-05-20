c6c0c80cdc36f79c0bc3eebd9cee36a75bf07a64
Midhun Devasia <mail@midhundevasia.com>
2017-05-20 09:59:08 +0530

    [BUGFIX] Fixed unexpected class loading issues in install tool

--- ext_localconf.php
+++ ext_localconf.php
@@ -30,15 +30,18 @@ defined('TYPO3_MODE') or die();
 	]
 );
 
-// Blog services like feed, xmlrpc etc
-\Tutorboy\Blogmaster\Service\BlogService::addService('feed', \Tutorboy\Blogmaster\Service\FeedService::class . '->generateBlogFeed');
-\Tutorboy\Blogmaster\Service\BlogService::addService('commentsFeed', \Tutorboy\Blogmaster\Service\FeedService::class . '->generateCommentsFeed');
-\Tutorboy\Blogmaster\Service\BlogService::addService('init', \Tutorboy\Blogmaster\Service\Core\GeneralService::class . '->activateFuturePosts');
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('blogmaster') && !TYPO3_REQUESTTYPE_INSTALL) {
 
-// Hooks
-//$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['encodeSpURL_postProc'][] = 'Tutorboy\\Blogmaster\\Hooks\\RealUrlHooks->encodeSpUrl_postProc';
-//$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['decodeSpURL_preProc'][] = 'Tutorboy\\Blogmaster\\Hooks\\RealUrlHooks->decodeSpUrl_preProc';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-postProcess'][$_EXTKEY] = 'Tutorboy\\Blogmaster\\Hooks\\PageRendererHooks->renderPostProcess';
+	// Blog services like feed, xmlrpc etc
+	\Tutorboy\Blogmaster\Service\BlogService::addService('feed', \Tutorboy\Blogmaster\Service\FeedService::class . '->generateBlogFeed');
+	\Tutorboy\Blogmaster\Service\BlogService::addService('commentsFeed', \Tutorboy\Blogmaster\Service\FeedService::class . '->generateCommentsFeed');
+	\Tutorboy\Blogmaster\Service\BlogService::addService('init', \Tutorboy\Blogmaster\Service\Core\GeneralService::class . '->activateFuturePosts');
+
+	// Hooks
+	//$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['encodeSpURL_postProc'][] = 'Tutorboy\\Blogmaster\\Hooks\\RealUrlHooks->encodeSpUrl_postProc';
+	//$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['decodeSpURL_preProc'][] = 'Tutorboy\\Blogmaster\\Hooks\\RealUrlHooks->decodeSpUrl_preProc';
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-postProcess'][$_EXTKEY] = 'Tutorboy\\Blogmaster\\Hooks\\PageRendererHooks->renderPostProcess';
+}
 
 if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('realurl')) {
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration'][$_EXTKEY]
